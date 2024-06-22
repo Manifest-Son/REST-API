8f96492b6b9c614beca96106dd0bd28230d4b672
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 14:36:09 +0300

    it implements URL route params

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ import user from "./data.js";
 
 const app = express()
 
-app.get("/users", getUsers)
-
-function getUsers(req, res){
-    res.status(200).json(user)
-};
+app.get("/users/:users_id", (req, res) => {
+    // console.log(req.params)
+    const users = user.filter(users => users.id == req.params.users_id)
+    res.send(users)
+})
 
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
