c8cada5f212c2d54ab5f28a05a98f3ae44ee742d
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 14:46:40 +0300

    it vaildates the URL params

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express()
 app.get("/users/:users_id", (req, res) => {
     // console.log(req.params)
     const users = user.filter(users => users.id == req.params.users_id)
-    res.send(users)
+    if(users.length === 0){
+        res.status(404).send("We are unable to find the data you requested.")
+    } else {
+        res.status(202).send(users)
+    }
+    
 })
 
 app.listen(3000,() => {
