c8034e3dc533b2f0827a7644d73ab258c3779fe0
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 11:50:33 +0300

    adds requests for an array

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/users" ,(request, response) => {
     response.send("You will get your users here.")
 })
 
+app.get("/songs", (req,res) => {
+    res.send(["Worthy","Oceans","Yesu Wastahili", "Mungu Yupo"])
+})
+
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
 })
