bbc13080fd2aebb5ac6f1bedbd6a1745f04a10af
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 14:00:47 +0300

    uses the popular response method and uses mock data.

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 import express, { request, response } from "express";
+import user from "./data.js";
 
 const app = express()
 
-app.get("/", (request, response) => {
-    response.send("You are seeing this in the endpoint")
-})
+app.get("/users", getUsers)
 
-app.get("/users" ,(request, response) => {
-    response.send("You will get your users here.")
-})
+function getUsers(req, res){
+    res.status(200).json(user)
+};
 
-const getSongs = (req,res) => {
-    res.send(["Worthy","Oceans","Yesu Wastahili", "Mungu Yupo"])
-}
-
-app.get("/songs", getSongs) ;
-
-app.post("/users", (req, res) => {
-    res.send("The user name will be added in the database")
-})
-
-app.patch("/users", (req, res) => {
-    res.send("The users list will be updated")
-})
-
-app.delete("/users", (req, res) => {
-    res.send("The user details is deleted")
-})
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
 })
