9a8244eb0ce3cdb28f888ff29820d08f774e0bfc
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 12:57:14 +0300

    adds ither types of requests such as POST, PATCH, DELETE. All these types were tested by the Insomnia API application.

--- index.js
+++ index.js
@@ -14,8 +14,19 @@ const getSongs = (req,res) => {
     res.send(["Worthy","Oceans","Yesu Wastahili", "Mungu Yupo"])
 }
 
-app.get("/songs", getSongs) 
+app.get("/songs", getSongs) ;
 
+app.post("/users", (req, res) => {
+    res.send("The user name will be added in the database")
+})
+
+app.patch("/users", (req, res) => {
+    res.send("The users list will be updated")
+})
+
+app.delete("/users", (req, res) => {
+    res.send("The user details is deleted")
+})
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
 })
