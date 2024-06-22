fbaddcc20d7a8b352fa5bed52431ce0d62933692
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 12:01:25 +0300

    it updates to separate the request handlers from the request itself.

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ app.get("/users" ,(request, response) => {
     response.send("You will get your users here.")
 })
 
-app.get("/songs", (req,res) => {
+const getSongs = (req,res) => {
     res.send(["Worthy","Oceans","Yesu Wastahili", "Mungu Yupo"])
-})
+}
+
+app.get("/songs", getSongs) 
 
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
