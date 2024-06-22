0257db0edd199507168801245a24433b5483f394
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 11:40:43 +0300

    makes a simple request

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express()
+
+app.get("/", (request, response) => {
+    response.send("You are seeing this in the endpoint")
+})
 
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
