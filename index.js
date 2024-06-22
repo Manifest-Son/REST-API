f3ed4c1553a3c4b0a7a0987de956a752a1b94774
Manifest-Son <lennoxgithinji37@gmail.com>
2024-06-22 11:44:28 +0300

    adds a new endpoint server request

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (request, response) => {
     response.send("You are seeing this in the endpoint")
 })
 
+app.get("/users" ,(request, response) => {
+    response.send("You will get your users here.")
+})
+
 app.listen(3000,() => {
     console.log("Server running on port 3000....")
 })
